bf0382a0a9375db9e87f64e15294a3da5a78b465
Lucas West <LWest.0J@gmail.com>
2024-03-25 10:10:10 -0400

    Innit

---
+++ 2-2-1_GoingTheDistance/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Lucas West                                                   #
+# 	Created:      3/25/2024, 10:09:17 AM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
