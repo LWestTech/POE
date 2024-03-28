b7c3452e225f63c02cb7a4c83dcbcaadccc03cae
Lucas West <LWest.0J@gmail.com>
2024-03-28 11:57:06 -0400

    03/28/24

--- 2-2-2_ChainReaction/src/main.py
+++ 2-2-2_ChainReaction/src/main.py
@@ -14,10 +14,21 @@ from vex import *
 brain=Brain()
 motor = Motor(Ports.PORT10)
 potentiometer = Potentiometer(brain.three_wire_port.a)
+bumper = Limit(brain.three_wire_port.c)
+motor_on = False
+def toggleMotor ():
+    global motor_on
+    motor_on = not motor_on
+
+bumper.pressed(toggleMotor)
 while True:
-    brain.screen.print(potentiometer.angle(PERCENT))
-    if (potentiometer.angle(PERCENT) > 25):
-        motor.spin(FORWARD, potentiometer.angle(PERCENT), PERCENT)
+    # brain.screen.print(potentiometer.angle(PERCENT))
+    brain.screen.print(motor_on)
+    print(motor_on)
+    # if (potentiometer.angle(PERCENT) > 25):
+    if (motor_on):
+        # motor.spin(FORWARD, potentiometer.angle(PERCENT), PERCENT)
+        motor.spin(FORWARD, 100, PERCENT)
     else:
         motor.stop()
     wait(30, MSEC)
