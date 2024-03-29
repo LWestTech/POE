fb4cab260b78a7c55f30c3961a7af6509cbde21d
Lucas West <LWest.0J@gmail.com>
2024-03-29 11:32:03 -0400

    Morning

---
+++ 2-2-3_MovingWithColor/src/main.py
@@ -0,0 +1,30 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Lucas West                                                   #
+# 	Created:      3/29/2024, 10:09:25 AM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+motor = Motor(Ports.PORT1)
+optical = Optical(Ports.PORT5)
+optical.set_light(LedStateType.ON)
+
+while True:
+    brain.screen.clear_screen()
+    brain.screen.set_cursor(1, 1)
+    hue = optical.hue()
+    if optical.is_near_object and hue is not None:
+        if hue < -30 and hue > 30:
+            brain.screen.print("RED")
+        if hue < 120 - 30 and hue > 120 + 30:
+            brain.screen.print("GREEN")
+        if hue < 240 - 30 and hue > 240 + 30:
+            brain.screen.print("BLUE")
+    wait(100, MSEC)
